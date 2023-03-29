a4a7bc1b931ea145bfb8b8f8c76c0e4ab664aeb0
BlakeJ07 <127786597+BlakeJ07@users.noreply.github.com>
2023-03-29 14:33:45 -0500

    Updated Player Jump

--- Player.gd
+++ Player.gd
@@ -1,7 +1,14 @@
 extends KinematicBody2D
 
+export(int) var JUMP_FORCE = -130
+export(int) var JUMP_RELEASE_FORCE = -70
+export(int) var MAX_SPEED = 50
+export(int) var ACCLERATION = 20
+export(int) var FRICTION = 20
+export(int) var GRAVITY = 4 
+export(int) var GRAVITY_FALL = 4
+
 var velocity = Vector2.ZERO
-var fast_fell = false
 
 func _physics_process(delta: float) -> void:
 	#Gravity
@@ -16,25 +23,23 @@ func _physics_process(delta: float) -> void:
 	
 	#Jump
 	if is_on_floor(): 
-		fast_fell = false
 		if Input.is_action_just_pressed("ui_up"):
-			velocity.y = -130
+			velocity.y = JUMP_FORCE
 	else: 
-		if Input.is_action_just_released("ui_up") and velocity.y < -70:
-			velocity.y = -70
+		if Input.is_action_just_released("ui_up") and velocity.y < JUMP_RELEASE_FORCE:
+			velocity.y = JUMP_RELEASE_FORCE
 		
-		if velocity.y > 0 and fast_fell:
-			velocity.y += 60
-			fast_fell = true
+		if velocity.y > 0:
+			velocity.y += GRAVITY_FALL
 	
 	#Apply movement to the player
 	velocity = move_and_slide(velocity, Vector2.UP)
 	
 func apply_gravity():
-	velocity.y += 4
+	velocity.y += GRAVITY
 
 func apply_friction():
-	velocity.x = move_toward(velocity.x, 0, 20)
+	velocity.x = move_toward(velocity.x, 0, FRICTION)
 
 func apply_acceleration(amount):
-	velocity.x = move_toward( velocity.x, 50 * amount, 20)
+	velocity.x = move_toward( velocity.x, MAX_SPEED * amount, ACCLERATION)
